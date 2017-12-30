af80e2d4e9e1cca6e54b66682386656c0babccc2
Pranitha <qa_ven@yahoo.com>
2017-12-29 19:02:16 -0600

    commiting calculator application with userdefined exceptions

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+package com.assignments.java;
+
+public interface Calculator  {
+
+	void sum() throws PositiveValuesException ;
+	void sub()throws PositiveValuesException;
+	void mul()throws PositiveValuesException;
+	void div()throws PositiveValuesException;
+	void mod()throws PositiveValuesException;
+}
